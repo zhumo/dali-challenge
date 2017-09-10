52a2b7add3f6556dda32566369b88e33b27376eb
Mo Zhu <mozhu888@gmail.com>
2017-09-10 12:54:54 -0400

    vue-resource to fetch json data

--- src/App.vue
+++ src/App.vue
@@ -14,24 +14,20 @@ export default {
   },
   data: function() {
     return {
-      members: [
-        {
-          "name": "Tim",
-          "iconUrl": "images/tim_round.jpg",
-          "url": "//www.zingweb.com",
-          "message": "Hiking in the High Sierras!",
-          "lat_long": [
-            37.2320967, -118.8578716
-          ],
-          "terms_on": [
-            "17W"
-          ],
-          "project": [
-            "Staff"
-          ]
-        }
-      ],
-    };
+      members: [],
+    }
+  },
+  created: function() {
+    this.$http.get(
+      'http://mappy.dali.dartmouth.edu/members.json'
+    ).then(
+      response => {
+        this.members = response.body
+      }, 
+      response => {
+        alert("Something went wrong:" + response.status)
+      },
+    )
   }
 }
 </script>
